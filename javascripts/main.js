513f712ab2b9b4e43701e0af931c27fba43eeb2a
RiPRR <rpr288@nyu.edu>
2019-05-24 01:21:48 -0400

    workin on it

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,39 @@
+$(document).ready(function(){
+	localStorage.setItem("tab","#info")
+	document.getElementById('projectPage').style.display = "none"
+	document.getElementById('aboutPage').style.display = "none"
+	document.getElementById('blogPage').style.display = "none"
+})
+
 $("#projects").click(function () {
-	console.log("FUCK")
-	$("#info").animate({width:"toggle",height:"toggle"})
+	deRender()
+	localStorage.setItem('tab','#projectPage');
+	$("#projectPage").animate({width:"toggle",height:"toggle"})
 });
+
+$("#home").click(function () {
+	deRender()
+	localStorage.setItem('tab','#info');
+	$("#info").animate({width:"toggle",height:"toggle"})
+	document.getElementById('info').visibility = "visibile"
+});
+
+$("#blog").click(function () {
+	deRender()
+	localStorage.setItem('tab','#blogPage');
+	$("#blogPage").animate({width:"toggle",height:"toggle"})
+});
+
+$("#about").click(function () {
+	deRender()
+	localStorage.setItem('tab','#aboutPage');
+	$("#aboutPage").animate({width:"toggle",height:"toggle"})
+});
+
+
+
+function deRender(){
+	let selector = localStorage.getItem("tab")
+	$(selector).animate({width:"toggle",height:"toggle"})
+	//setTimeout(function(){console.log("waiting"),7000})
+}
