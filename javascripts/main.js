88bf6df1d7e6bd596bfcde785aa623ccd96f913f
RiPRR <rpr288@nyu.edu>
2020-04-03 00:58:14 -0400

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,13 +2,23 @@ let curSection = 'about'
 function onload() {
     let firstEle = document.querySelector('.nav').children[0]
     curSection = firstEle
-    curSection.style.opacity = .5;  
+    curSection.style.opacity = .5;
+
 }
-function sectionSwap(targetSection){
-    if(targetSection !== curSection){
+function sectionSwap(targetSection) {
+    if (targetSection !== curSection) {
         curSection.style.opacity = .2
         targetSection.style.opacity = .5
         curSection = targetSection
+        let sections = document.querySelector('.sections').children
+        let visibleSection = targetSection.id
+        for (let section of sections) {
+            if (section.className !== visibleSection) {
+                section.style.display = 'none'
+            }
+            else {
+                section.style.display = ''
+            }
+        }
     }
-
 }
